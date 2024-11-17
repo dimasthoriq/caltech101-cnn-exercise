2b925313d5dd53eae4548de6ab9cee10ac968e07
Dimas <dimasat15@gmail.com>
2024-11-17 23:41:24 +0900

    update experiment.py

--- experiment.py
+++ experiment.py
@@ -12,10 +12,10 @@ config = {
     'compression': None,
 
     # Training designs
-    'dropout_rate': 0.,
+    'dropout_rate': 0.2,
     'weight_decay': 2e-5,
-    'loss': 'CE',
-    'from_scratch': 'imagenet',
+    'loss': 'ce',
+    'from_scratch': 'tune',
     'learning_rate': 1e-1,
     'batch_size': 32,
 
@@ -44,6 +44,7 @@ from_scratch: ['imagenet', 'random', 'tune']
 learning_rate: [1e-1, 1e-2, 1e-4]
 batch_size: [32, 16, 8]
 """
+
 
 def experiment(config):
     if config['archi'] == 'efficientnet_v2_s':
@@ -80,3 +81,45 @@ def experiment(config):
     print(f'Evaluating on test set...')
     evaluate(trainer.model, test_loader, config)
     print(f'Finished!')
+
+
+for archi in ['custom', 'efficientnet_v2_s', 'resnet101']:
+    print(f'Running experiments for {archi} architecture...')
+    config['archi'] = archi
+    if archi == 'custom':
+        for normalization in ['group', 'layer', 'batch']:
+            print(f'Running experiments for {normalization} normalization...')
+            config['normalization'] = normalization
+            experiment(config)
+
+        for compression in [128, None]:
+            print(f'Running experiments for {compression} compression...')
+            config['compression'] = compression
+            experiment(config)
+
+        for dropout_rate in [0., 0.2]:
+            print(f'Running experiments for {dropout_rate} dropout rate...')
+            config['dropout_rate'] = dropout_rate
+            experiment(config)
+
+    else:
+        for from_scratch in ['random', 'imagenet', 'tune']:
+            print(f'Running experiments for {from_scratch} initialization...')
+            config['from_scratch'] = from_scratch
+            experiment(config)
+
+    for weight_decay in [0., 2e-5]:
+        config['weight_decay'] = weight_decay
+        experiment(config)
+
+    for loss in ['squared_hinge', 'ce']:
+        config['loss'] = loss
+        experiment(config)
+
+    for learning_rate in [1e-4, 1e-2, 1e-1]:
+        config['learning_rate'] = learning_rate
+        experiment(config)
+
+    for batch_size in [8, 16, 32]:
+        config['batch_size'] = batch_size
+        experiment(config)
